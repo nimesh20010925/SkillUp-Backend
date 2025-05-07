233599be0bb3c6dcf1e3438a451c53b517b79efa
Tharusha Rashmika Sandakelum <103655595+Tharusha200219@users.noreply.github.com>
2025-05-07 21:17:46 +0530

    Add comment for imports in LearningPlanController

--- demo/src/main/java/com/skillup/demo/controller/LearningPlanController.java
+++ demo/src/main/java/com/skillup/demo/controller/LearningPlanController.java
@@ -2,7 +2,7 @@ package com.skillup.demo.controller;
 
 import java.util.List;
 import java.util.Optional;
-
+// Imports for handling Java collections, optional values, and Spring framework annotations
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
