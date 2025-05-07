c7f1bc6786e1772ef7880e84d6f10e3e64a5669b
Tharusha Rashmika Sandakelum <103655595+Tharusha200219@users.noreply.github.com>
2025-05-07 21:18:43 +0530

    Add  ResponseEntity import in LearningPlanController

--- demo/src/main/java/com/skillup/demo/controller/LearningPlanController.java
+++ demo/src/main/java/com/skillup/demo/controller/LearningPlanController.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 // Imports for handling Java collections, optional values, and Spring framework annotations
 import org.springframework.beans.factory.annotation.Autowired;// For dependency injection
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity;// For HTTP response handling
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
