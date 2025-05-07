3374d4bd45f4843d502b865c6adfd290dac661dd
Himasha20011106 <133731857+Himasha20011106@users.noreply.github.com>
2025-05-07 19:00:51 +0530

    Added changes for inserting image posts and video posts

--- demo/src/main/java/com/skillup/demo/controller/PostController.java
+++ demo/src/main/java/com/skillup/demo/controller/PostController.java
@@ -140,7 +140,8 @@ public class PostController {
 		
 		return new ResponseEntity<>(res,HttpStatus.OK);
 	}
-	
+	// Get all posts by a specific user ID
+	// Get all posts by a specific user ID
 	// Get all posts by a specific user ID
 	@PutMapping("/edit")
 	public ResponseEntity<MessageResponse> editPostHandler(@RequestBody Post post) throws PostException{
